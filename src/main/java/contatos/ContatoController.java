cfc18f21d60226a4ffdba2f827541aed929e0320
RafaeldeCarvalhoBZR <84476936+RafaeldeCarvalhoBZR@users.noreply.github.com>
2022-12-28 14:18:55 -0300

    html

--- src/main/java/contatos/ContatoController.java
+++ src/main/java/contatos/ContatoController.java
@@ -4,13 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class ContatoController {
@@ -23,15 +27,18 @@ public class ContatoController {
 		return repository.findById(id).get();
 	}
 
-	@GetMapping("/contato")
-	public List<Contato> getAllContatos() {
-		return repository.findAll();
+	@GetMapping("/contatos")
+	public ModelAndView getAllContatos() {
+		List<Contato> lista = repository.findAll();
+		ModelAndView modelAndView = new ModelAndView("contatos.html");
+        modelAndView.addObject("contatos", lista);
+        return modelAndView;
 	}
-
-
-	@PostMapping("/contato")
-	public Contato saveContato(@RequestBody Contato contato) {
-		return repository.save(contato);
+	
+	@PostMapping("/contatos")
+	public RedirectView saveContato(Contato contato) {
+		  repository.save(contato);
+		  return new RedirectView("/contatos", true);
 	}
 
 	@DeleteMapping("/contato/{id}")
